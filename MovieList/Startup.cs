c97229d3f04e818559ccb1987fcfb4bce73efe95
Nick Garrels <nrgarrels@dmacc.edu>
2023-06-07 20:00:49 -0500

    Initial

---
+++ MovieList/Startup.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+using MovieList.Models;
+
+public class Startup
+{
+    public Startup(IConfiguration configuration)
+    {
+        Configuration = configuration;
+
+
+
+    }
+    public IConfiguration Configuration { get; }
+    public void ConfigureServices(IServiceCollection services)
+    {
+       services.AddDbContext<MovieContext>(options => options.UseSqlServer(Configuration.GetConnectionString("MovieContext")));
+        services.AddRouting(options => {
+            options.LowercaseUrls = true; 
+            options.AppendTrailingSlash = true;
+        });
+
+       // app.UseEndpoints(endpoints => {
+       //     endpoints.MapControllerRoute(name: "default",
+        //    pattern: "{controller=Home}/{action=Index}/{id?}/{slug?}");
+      //  });
+
+    }
+
+}
+
